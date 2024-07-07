60422ce0ab5c4e8784f51cb289e0806981791048
Lionel TOTON <totonlionel@gmail.com>
2024-07-07 20:37:50 +0200

    need used of python

---
+++ size-image.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import os
+
+def reduce_image_size(input_image_path, output_image_path, percent_reduction):
+    # Ouvre l'image
+    with Image.open(input_image_path) as img:
+        # Calculer les nouvelles dimensions
+        width, height = img.size
+        new_width = int(width * (1 - percent_reduction / 100))
+        new_height = int(height * (1 - percent_reduction / 100))
+        
+        # Redimensionne l'image
+        resized_img = img.resize((new_width, new_height))
+        
+        # Enregistre l'image redimensionnée
+        resized_img.save(output_image_path)
+        
+        # Affiche les informations sur la taille avant et après le redimensionnement
+        original_size = os.path.getsize(input_image_path) / (1024 * 1024)  # Taille originale en Mo
+        resized_size = os.path.getsize(output_image_path) / (1024 * 1024)  # Taille redimensionnée en Mo
+        print(f"Image originale : {original_size:.2f} Mo")
+        print(f"Image redimensionnée : {resized_size:.2f} Mo")
+
+# Chemin de l'image d'entrée et de sortie
+# input_image_path = 'chemin/vers/votre/image.jpg'
+# input_image_path = r'C:\Users\HP\Documents\impactandpartners\Application_Web_Plan_Comptable_Impact_And_Partners\frontend\dashboard\static\profildashboard\non_lucratif.jpg'
+
+input_image_path = r'C:\Users\HP\Pictures\IMAGE_REZIZER\sign-in-page.png'
+
+output_image_path = r'C:\Users\HP\Pictures\IMAGE_REZIZER\rezizer\sign-in-page.png'
+
+# output_image_path = r'C:\Users\HP\Documents\impactandpartners\Application_Web_Plan_Comptable_Impact_And_Partners\frontend\dashboard\static\profildashboard\reduce\non_lucratif.jpg'
+
+# Pourcentage de réduction de taille
+percent_reduction = 50
+
+# Appel de la fonction pour réduire la taille de l'image
+reduce_image_size(input_image_path, output_image_path, percent_reduction)
---
+++ size.py
@@ -0,0 +1,26 @@
+import os
+
+def get_size(path):
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    elif os.path.isdir(path):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(path):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size
+    return 0
+
+def list_directory_by_size(directory_path):
+    items = [(item, get_size(os.path.join(directory_path, item))) for item in os.listdir(directory_path)]
+    sorted_items = sorted(items, key=lambda x: x[1], reverse=True)
+    for item, size_bytes in sorted_items:
+        size_mb = size_bytes / (1024 * 1024)  # Convert bytes to megabytes
+        print(f"{item}: {size_mb:.2f} MB")
+
+# Définissez le chemin du répertoire
+directory_path = r'C:\Users\HP\Documents\impactandpartners\Application_Web_Plan_Comptable_Impact_And_Partners\frontend\dist'
+
+# Liste les éléments du répertoire par taille décroissante en Mo
+list_directory_by_size(directory_path)
